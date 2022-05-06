cd6b9b62bed198e74b46f0e60b72450a81df41a4
teo.yu <developer.1px@gmail.com>
2022-05-06 11:05:33 +0900

    ✨ 내용 업데이트 🎨 프로필 색상 해시 추가

--- src/components/Talks.svelte
+++ src/components/Talks.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 export let data = []
 const dates = data.filter(t => t.type === "(날짜)").reverse()
+const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0)
+const nickToColor = (nickname) => Math.abs(hashCode(nickname) % 360)
 </script>
 
 <div class="flex bg(#fff) hbox gap(10) p(20) scroll-x font(10) fixed top(60) w(100%) none .isUp:hbox!">
@@ -20,18 +22,21 @@ const dates = data.filter(t => t.type === "(날짜)").reverse()
         <div class="hbox(top) gap(10) mt(8)">
           <div class="flex pl(40)">
             <div class="hbox(bottom) gap(4)">
-              <p class="p(12/16) font(16) bg(#f1f1f1) r(10) pre w(~100%-50px) >>a:color(#4f80ff) >>a:hover:underline">{@html row.text}</p>
+              <p class="p(12/16) font(14) bg(#f1f1f1) r(10) pre w(~100%-50px) >>a:color(#4f80ff) >>a:hover:underline">{@html row.text}</p>
               <div class="font(10) c(#888)">{row.created_at?.[1]}</div>
             </div>
           </div>
         </div>
       {:else if row.text}
         <div class="hbox(top) gap(10) mt(24)">
-          <div class="w(32) h(32) bg(hsl(100,75%,75%)) r(2) pack font(12) nowrap">•‿•</div>
+          <div>
+            <div class="w(32) h(32) bg(hsl(100,75%,75%)) r(2) pack font(12) nowrap" style:background-color="hsl({nickToColor(row.nickname)},75%,75%)">•‿•</div>
+          </div>
+
           <div class="flex">
             <div class="font(11)">{row.nickname}</div>
             <div class="hbox(bottom) gap(4)">
-              <p class="p(12/16) font(16) bg(#f1f1f1) r(10) rtl(0) pre w(~100%-50px) >>a:color(#4f80ff) >>a:hover:underline">{@html row.text}</p>
+              <p class="p(12/16) font(14) bg(#f1f1f1) r(10) rtl(0) pre w(~100%-50px) >>a:color(#4f80ff) >>a:hover:underline">{@html row.text}</p>
               <div class="font(10) c(#888)">{row.created_at?.[1]}</div>
             </div>
           </div>
@@ -41,13 +46,13 @@ const dates = data.filter(t => t.type === "(날짜)").reverse()
       {#if row?.ogs?.filter(Boolean).length}
         <div class="pl(40)">
           {#each (row?.ogs?.filter(Boolean) || []) as {images, title, description, url}}
-            <a href={url} target="_blank" class="vbox mt(8) w(600) w(~100%) h(200~) bg(#fff) b(#000.1) r(8) clip transition(.3s) hover:box-shadow(0/0/10px/#000.1) pointer">
+            <a href={url} target="_blank" class="vbox mt(8) w(320) w(~100%) h(200~) bg(#fff) b(#000.1) r(8) clip transition(.3s) hover:box-shadow(0/0/10px/#000.1) pointer">
               {#each (images ?? []).slice(0, 1) as img}
-                <img class="cover h(~480)" src={img} on:error={e => e.target.style.display='none'}/>
+                <img class="cover" src={img} on:error={e => e.target.style.display='none'}/>
               {/each}
               <div class="p(16)">
-                <div class="bold">{title || url}</div>
-                <div class="c(#999) font(13)">
+                <div class="font(14) bold">{title || url}</div>
+                <div class="c(#999) font(12)">
                   <div>{description || ''}</div>
                 </div>
               </div>
